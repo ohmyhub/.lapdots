1758e708256d69ee247e6bfffa9711c3e4609344
ohmyhub <k.warden.89@gmail.com>
2022-02-16 22:33:20 -0800

    fish: Add a bunch of wayland env variables

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,14 +1,46 @@
 if status is-interactive
-    set -gx fish_greeting ""
+
+    # swayland
+    set -gx QT_QPA_PLATFORM wayland-egl
     set -gx MOZ_ENABLE_WAYLAND 1
-    set -gx RANGER_LOAD_DEFAULT_RC FALSE
+    set -gx MOZ_DBUS_REMOTE 1
+    set -gx MOZ_WEBRENDER 1
+    set -gx MOZ_ACCELERATED 1
+    set -gx XDG_CURRENT_DESKTOP sway
+    set -gx XDG_SESSION_DESKTOP sway
+    set -gx XDG_SESSION_TYPE wayland
+    set -gx CLUTTER_BACKEND wayland
+    set -gx ECORE_EVAS_ENGINE wayland-egl
+    set -gx ELM_ENGINE wayland_egl
+    set -gx SDL_VIDEODRIVER wayland
+    set -gx _JAVA_AWT_WM_NONREPARENTING 1
+    set -gx NO_AT_BRIDGE 1
+    set -gx STATUSBAR waybar
+    set -gx SWAY_CURSOR_SIZE 16
+
+    # hush now
+    set -gx fish_greeting ""
+
+    # fzf
     set -gx FZF_DEFAULT_OPTS "--cycle --layout=reverse --border --height=90% --marker='*' --exact"
     set -gx FZF_DEFAULT_COMMAND "rg --files --hidden --follow --glob '!.git'"
+
+    # ranger
+    set -gx RANGER_LOAD_DEFAULT_RC FALSE
+
+    # node
     set -gx npm_config_prefix /home/kevin/.local
-    set -gx XDG_CURRENT_DESKTOP sway
-    set -gx QT_QPA_PLATFORM wayland
+
+    # iGPU
     set -gx LIBVA_DRIVER_NAME iHD
     set -gx VDPAU_DRIVER va_gl
+
+    # user dirs
+    set -gx XDG_PUBLICSHARE_DIR /home/kevin
+    set -gx XDG_TEMPLATES_DIR /home/kevin
+    set -gx XDG_DESKTOP_DIR /home/kevin
+
+    # misc
     set -gx MANPAGER "fish --command 'col --no-backspaces --spaces | bat --language man --plain'" # use bat to colourise man
 
     # TokyoNight Color Palette
