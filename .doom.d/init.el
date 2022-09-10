9b4ca14ee70a7e02f37de66b8a62ba6c5d69b03a
ohmyhub <k.warden.89@gmail.com>
2022-09-10 08:28:05 -0700

    doom: Prettify the git fringe

--- .doom.d/init.el
+++ .doom.d/init.el
@@ -50,7 +50,9 @@
        tabs                ; a tab bar for Emacs
        ;;treemacs          ; a project drawer, like neotree but cooler
        unicode             ; extended unicode support for various languages
-       vc-gutter           ; vcs diff in the fringe
+       (vc-gutter          ; vcs diff in the fringe
+        +diff-hl
+        +pretty)
        ;;vi-tilde-fringe   ; fringe tildes to mark beyond EOB
        (window-select      ; visually switch windows
         +numbers)
